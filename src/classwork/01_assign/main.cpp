ade6d9e2b948ba1d7017f207c70ec1b4b4298a31
luism7171 <luis.martinez23@g.austincc.edu>
2021-02-06 05:56:41 +0000

    I did homework one but my executable does not want to run im pretty sure everything is right tho

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -1,11 +1,25 @@
 //Write the include statement for types.h here
 
 //Write include for capturing input from keyboard and displaying output to screen
-
+#include "types.h"
+#include <iostream>
+using std::cout; using std::cin;
 
 
 int main() 
 {
+  int num;
+  cin>>num;
+  int result;
+  result = multiply_numbers(num);
+  cout<<result;
+  int num1 = 4;
+  result = multiply_numbers(num1);
+  cout<<result;
+  
+
+
+  
 
 	return 0;
 }
